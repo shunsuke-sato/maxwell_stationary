f2b5c06dd47e93596717cdeda8d1663d84cd0059
Shunsuke Sato <shunsuke.sato26@gmail.com>
2021-04-07 16:19:01 +0200

    Implemented the spatial dependence analysis.

--- maxwell.f90
+++ maxwell.f90
@@ -12,31 +12,64 @@ module global_variables
   real(8),parameter :: volume = 112.0394d0
 
   integer,parameter :: nene = 4
-  real(8),parameter :: Eex_surface = (0.1d0/ev)/volume
+  real(8) :: Eex_surface = 0d0 !(0.1d0/ev)/volume
   complex(8) :: zeps_w(0:nw,nene)
   real(8) :: Eene(nene), Temp(nene)
 
 
   real(8) :: Eex_x(0:nx),Temp_x(0:nx)
-
+  complex(8) :: zeps_x(0:nx)
 
 end module global_variables
 !-------------------------------------------------------------------------------
 program main
   use global_variables
   implicit none
-  complex(8) :: Ez(0:nx), dEz_dx(0:nx), zeps(0:nx), zeps_dx2(0:nx)
-  complex(8) :: Ez_0, dEz_dx_0
-  complex(8) :: zk1, zk2, zk3, zk4
-  complex(8) :: zl1, zl2, zl3, zl4
-  real(8) :: omega, kx
-  integer :: ix
+  integer :: iw
+  real(8) :: ww, reflectivity, transmission
+  real(8) :: ref_gs(0:nw), tra_gs(0:nw)
+  real(8) :: ref_pp(0:nw), tra_pp(0:nw)
 
 
 
   call setup_dielectric_function
+
+  Eex_surface = 0d0 !(0.1d0/ev)/volume
   call calc_energy_distribution
 
+  open(30,file='ref_trans_gs.out')
+  do iw = 0, nw
+    ww = wi + dw*iw
+    call setup_dielectric_function_in_real_space(iw)
+    call calc_maxwell(iw, reflectivity, transmission)
+    ref_gs(iw) = reflectivity
+    tra_gs(iw) = transmission
+    write(30,"(999e26.16e3)")ww,reflectivity,transmission
+  end do
+  close(30)
+
+
+  Eex_surface = (0.5d0/ev)/volume
+  call calc_energy_distribution
+
+  open(30,file='ref_trans_pp.out')
+  do iw = 0, nw
+    ww = wi + dw*iw
+    call setup_dielectric_function_in_real_space(iw)
+    call calc_maxwell(iw, reflectivity, transmission)
+    ref_pp(iw) = reflectivity
+    tra_pp(iw) = transmission
+    write(30,"(999e26.16e3)")ww,reflectivity,transmission
+  end do
+  close(30)
+  
+
+  open(30,file='transient_abs.out')
+  do iw = 0, nw
+    ww = wi + dw*iw
+    write(30,"(999e26.16e3)")ww,-log(tra_pp(iw)/tra_gs(iw))
+  end do
+  close(30)
 
 !  zeps = 1d0
 !  zeps_dx2 = 1d0
@@ -76,6 +109,55 @@ program main
 
   
 end program main
+!-------------------------------------------------------------------------------
+subroutine calc_maxwell(iw, reflectivity, transmission)
+  use global_variables
+  implicit none
+  integer,intent(in) :: iw
+  real(8),intent(out) :: reflectivity, transmission
+  real(8) :: ww, k0
+  complex(8) :: Ez, dEz_dx
+  complex(8) :: Ez_0, dEz_dx_0
+  complex(8) :: zeps_mid
+  complex(8) :: zk1, zk2, zk3, zk4
+  complex(8) :: zl1, zl2, zl3, zl4
+  integer :: ix
+  complex(8) :: za, zr, zt
+
+
+  ww = wi + dw*iw
+  k0 = ww/clight
+
+  Ez = 1d0
+  dEz_dx = zi*k0*Ez
+  zt = Ez
+
+  do ix = 0, nx-1
+    zk1 = -k0**2*zeps_x(nx-ix)*Ez
+    zl1 = dEz_dx
+
+    zeps_mid = 0.5d0*(zeps_x(nx-ix)+zeps_x(nx-ix-1))
+    zk2 = -k0**2*zeps_mid*(Ez-0.5d0*dx*zl1)
+    zl2 = dEz_dx -0.5d0*dx*zk1
+
+    zk3 = -k0**2*zeps_mid*(Ez-0.5d0*dx*zl2)
+    zl3 = dEz_dx -0.5d0*dx*zk2
+
+    zk4 = -k0**2*zeps_x(nx-ix-1)*(Ez-dx*zl3)
+    zl4 = dEz_dx -dx*zk3
+
+    dEz_dx = dEz_dx -dx/6d0*(zk1+2d0*zk2+2d0*zk3+zk4)
+    Ez = Ez -dx/6d0*(zl1+2d0*zl2+2d0*zl3+zl4)
+
+  end do
+
+  za = 0.5d0*(Ez+dEz_dx/(zi*k0))
+  zr = 0.5d0*(Ez-dEz_dx/(zi*k0))
+
+  reflectivity = abs(zr/za)**2
+  transmission = abs(zt/za)**2
+
+end subroutine calc_maxwell
 !-------------------------------------------------------------------------------
 subroutine setup_dielectric_function
   use global_variables
@@ -140,7 +222,6 @@ subroutine setup_dielectric_function
   end do
 
 
-
 end subroutine setup_dielectric_function
 !-------------------------------------------------------------------------------
 subroutine calc_energy_distribution
@@ -191,6 +272,38 @@ subroutine calc_energy_distribution
   close(30)
 
 end subroutine calc_energy_distribution
+!-------------------------------------------------------------------------------
+subroutine setup_dielectric_function_in_real_space(iw)
+  use global_variables
+  implicit none
+  integer,intent(in) :: iw
+  integer,parameter :: Np = nene
+  real(8) :: xn(0:Np-1)
+  real(8) :: yn_r(0:Np-1),an_r(0:Np-2),bn_r(0:Np-2),cn_r(0:Np-2),dn_r(0:Np-2)
+  real(8) :: yn_i(0:Np-1),an_i(0:Np-2),bn_i(0:Np-2),cn_i(0:Np-2),dn_i(0:Np-2)
+  integer :: iene, ix
+  real(8) :: sr, si, xx
+
+
+  do iene = 1, nene
+    xn(iene-1)    = Eene(iene)
+    yn_r(iene-1)  = real( zeps_w(iw,iene))
+    yn_i(iene-1)  = aimag(zeps_w(iw,iene))
+  end do
+
+  call spline(Np,xn,yn_r,an_r,bn_r,cn_r,dn_r)
+  call spline(Np,xn,yn_i,an_i,bn_i,cn_i,dn_i)
+
+  zeps_x = 0d0
+
+  do ix = 0, nx
+    xx = dx*ix
+    call calc_signal(Np,xn,yn_r,an_r,bn_r,cn_r,dn_r,Eex_x(ix),sr)
+    call calc_signal(Np,xn,yn_i,an_i,bn_i,cn_i,dn_i,Eex_x(ix),si)
+    zeps_x(ix) = sr + zi*si
+  end do
+
+end subroutine setup_dielectric_function_in_real_space
 !-------------------------------------------------------------------------------
 subroutine spline(Np,xn,yn,an,bn,cn,dn)
   implicit none
